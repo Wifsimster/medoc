b2a93c5c967398fcc96b5f8c32bbbc9847978bbe
Wifsimster <battistella@gmail.com>
2019-08-09 08:15:05 +0200

    commt

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const Medoc = require('./medoc')
+const Medoc = require("./medoc")
 
 // const PATH_TO_SCAN = `w:`
 // const PATH_TO_PUSH = `z:`
 
-const PATH_TO_SCAN = `D:\\hexawin8`
-const PATH_TO_PUSH = `D:\\hexawin8`
+// const PATH_TO_SCAN = `D:\\hexawin8`
+// const PATH_TO_PUSH = `D:\\hexawin8`
 
 const medoc = new Medoc(PATH_TO_SCAN, PATH_TO_PUSH)
 
