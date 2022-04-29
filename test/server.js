2851ccea5b2776481a78756012485c8df63322f7
Wifsimster <wifsimster@gmail.com>
2022-04-29 21:26:45 +0200

    feat: add run sync method

--- test/server.js
+++ test/server.js
@@ -1,16 +1,18 @@
-const Medoc = require("../medoc")
+const Medoc = require("../medoc");
 
-const PATH_TO_SCAN = `w:`
-const PATH_TO_PUSH = `z:`
+// const PATH_TO_SCAN = `w:`
+// const PATH_TO_PUSH = `z:`
+const PATH_TO_SCAN = `u:`;
+const PATH_TO_PUSH = `q:`;
 
 // const episodes = Medoc.search(PATH_TO_SCAN, PATH_TO_PUSH)
 
-const medoc = new Medoc(PATH_TO_SCAN, PATH_TO_PUSH)
+const medoc = new Medoc(PATH_TO_SCAN, PATH_TO_PUSH);
 medoc
-  .run()
-  .then(results => {
-    console.log(results)
+  .runSync()
+  .then((results) => {
+    console.log("Terminé :", results);
   })
-  .catch(err => {
-    console.error(err)
-  })
+  .catch((err) => {
+    console.error(err);
+  });
