0c1d9026e70b98b12791d85618e4a7fb309bf9df
Wifsimster <wifsimster@gmail.com>
2023-02-26 14:46:26 +0100

    chore: update

--- medoc.js
+++ medoc.js
@@ -3,305 +3,303 @@
  * Licensed under MIT, https://opensource.org/licenses/MIT/
  */
 
-const fs = require("fs");
-const path = require("path");
-const mkdirp = require("mkdirp");
-const rimraf = require("rimraf");
+const fs = require("fs")
+const path = require("path")
+const mkdirp = require("mkdirp")
+const rimraf = require("rimraf")
 
 const VIDEO_FORMATS = [
-  ".avi",
-  ".mkv",
-  ".mp4",
-  ".webm",
-  ".flv",
-  ".vob",
-  ".ogg",
-  ".amv",
-];
+	".avi",
+	".mkv",
+	".mp4",
+	".webm",
+	".flv",
+	".vob",
+	".ogg",
+	".amv",
+]
 
 module.exports = class Medoc {
-  constructor(from, to) {
-    this.from = from;
-    this.to = to;
-  }
+	constructor(from, to) {
+		this.from = from
+		this.to = to
+	}
 
-  async run() {
-    const episodes = Medoc.search(this.from, this.to);
+	async run() {
+		const episodes = Medoc.search(this.from, this.to)
 
-    if (Array.isArray(episodes)) {
-      return await Promise.all(
-        episodes.map(async (episode) => {
-          return await Medoc.move(episode);
-        })
-      );
-    } else {
-      return await Promise.reject(episodes);
-    }
-  }
+		if (Array.isArray(episodes)) {
+			return await Promise.all(
+				episodes.map(async (episode) => {
+					return await Medoc.move(episode)
+				})
+			)
+		} else {
+			return await Promise.reject(episodes)
+		}
+	}
 
-  async runSync() {
-    const episodes = Medoc.search(this.from, this.to);
+	async runSync() {
+		const episodes = Medoc.search(this.from, this.to)
 
-    if (Array.isArray(episodes)) {
-      for (let index = 0; index <= episodes.length - 1; index++) {
-        let episode = episodes[index];
-        if (episode && episode.episode) {
-          console.info(`Moving '${episode.episode.fullname}'`);
-          await Medoc.move(episode);
-        } else {
-          console.error("Episode is undefined !");
-        }
-      }
-      return episodes;
-    } else {
-      return await Promise.reject(episodes);
-    }
-  }
+		if (Array.isArray(episodes)) {
+			for (let index = 0; index <= episodes.length - 1; index++) {
+				let episode = episodes[index]
+				if (episode && episode.episode) {
+					console.info(`Moving '${episode.episode.fullname}'`)
+					await Medoc.move(episode)
+				} else {
+					console.error("Episode is undefined !")
+				}
+			}
+			return episodes
+		} else {
+			return await Promise.reject(episodes)
+		}
+	}
 
-  static async move(episode) {
-    const sourcePath = episode.origin.path;
-    const destinationPath = episode.destination.path;
-    const destinationDirectory = Medoc.getDestinationDirectory(episode);
+	static async move(episode) {
+		const sourcePath = episode.origin.path
+		const destinationPath = episode.destination.path
+		const destinationDirectory = Medoc.getDestinationDirectory(episode)
 
-    if (episode.origin.isDirectory) {
-      const sourceDirectory = Medoc.getOriginDirectory(episode);
+		if (episode.origin.isDirectory) {
+			const sourceDirectory = Medoc.getOriginDirectory(episode)
 
-      if (Medoc.hasFile(sourceDirectory)) {
-        if (!fs.existsSync(destinationDirectory)) {
-          Medoc.createDirectory(destinationDirectory);
-        }
+			if (Medoc.hasFile(sourceDirectory)) {
+				if (!fs.existsSync(destinationDirectory)) {
+					Medoc.createDirectory(destinationDirectory)
+				}
 
-        return await new Promise((resolve, reject) => {
-          var reader = fs.createReadStream(sourcePath);
+				return await new Promise((resolve, reject) => {
+					var reader = fs.createReadStream(sourcePath)
 
-          reader.on("open", () => {
-            let writer = fs.createWriteStream(destinationPath);
-            reader.pipe(writer);
-          });
+					reader.on("open", () => {
+						let writer = fs.createWriteStream(destinationPath)
+						reader.pipe(writer)
+					})
 
-          reader.on("close", async () => {
-            await Medoc.removePath(sourceDirectory);
-            resolve({ origin: sourcePath, destination: destinationPath });
-          });
-        });
-      } else {
-        await Medoc.removePath(sourceDirectory);
-        console.log(`${sourceDirectory} directory has no video file !`);
-        resolve({
-          message: `${sourceDirectory} directory has no video file !`,
-        });
-      }
-    }
+					reader.on("close", async () => {
+						await Medoc.removePath(sourceDirectory)
+						resolve({ origin: sourcePath, destination: destinationPath })
+					})
+				})
+			} else {
+				await Medoc.removePath(sourceDirectory)
+				console.log(`${sourceDirectory} directory has no video file !`)
+				throw new Error({ message: `${sourceDirectory} directory has no video file !` })
+			}
+		}
 
-    if (episode.origin.isFile) {
-      if (!fs.existsSync(destinationDirectory)) {
-        Medoc.createDirectory(destinationDirectory);
-      }
+		if (episode.origin.isFile) {
+			if (!fs.existsSync(destinationDirectory)) {
+				Medoc.createDirectory(destinationDirectory)
+			}
 
-      return await new Promise((resolve, reject) => {
-        var reader = fs.createReadStream(sourcePath);
+			return await new Promise((resolve, reject) => {
+				var reader = fs.createReadStream(sourcePath)
 
-        reader.on("open", () => {
-          let writer = fs.createWriteStream(destinationPath);
-          reader.pipe(writer);
-        });
+				reader.on("open", () => {
+					let writer = fs.createWriteStream(destinationPath)
+					reader.pipe(writer)
+				})
 
-        reader.on("close", async () => {
-          await Medoc.removePath(episode.origin.file);
-          resolve({ origin: sourcePath, destination: destinationPath });
-        });
-      });
-    }
-  }
+				reader.on("close", async () => {
+					await Medoc.removePath(episode.origin.file)
+					resolve({ origin: sourcePath, destination: destinationPath })
+				})
+			})
+		}
+	}
 
-  static removePath(url) {
-    return new Promise((resolve, reject) => {
-      rimraf(url, () => {
-        resolve(`${url} removed !`);
-      });
-    });
-  }
+	static removePath(url) {
+		return new Promise((resolve, reject) => {
+			rimraf(url, () => {
+				resolve(`${url} removed !`)
+			})
+		})
+	}
 
-  // Create directory and subdirectories if necessary
-  static createDirectory(url) {
-    if (!fs.existsSync(url)) {
-      mkdirp.sync(url);
-      return true;
-    }
-    return true;
-  }
+	// Create directory and subdirectories if necessary
+	static createDirectory(url) {
+		if (!fs.existsSync(url)) {
+			mkdirp.sync(url)
+			return true
+		}
+		return true
+	}
 
-  static isEpisode(filename) {
-    if (/([sS]\d{2}[eE]\d{2})/g.exec(filename)) {
-      return true;
-    }
-    return false;
-  }
+	static isEpisode(filename) {
+		if (/([sS]\d{2}[eE]\d{2})/g.exec(filename)) {
+			return true
+		}
+		return false
+	}
 
-  static hasFile(url) {
-    let files = fs.readdirSync(url);
-    let filteredFiles = files.filter((file) => {
-      if (VIDEO_FORMATS.includes(path.extname(file))) {
-        return true;
-      }
-    });
-    if (filteredFiles[0]) {
-      return true;
-    }
-    return false;
-  }
+	static hasFile(url) {
+		let files = fs.readdirSync(url)
+		let filteredFiles = files.filter((file) => {
+			if (VIDEO_FORMATS.includes(path.extname(file))) {
+				return true
+			}
+		})
+		if (filteredFiles[0]) {
+			return true
+		}
+		return false
+	}
 
-  static getYear(filename) {
-    let rst = /(\d{4})/.exec(filename);
-    if (rst && rst[0] !== "1080") {
-      return rst[0];
-    }
-    return null;
-  }
+	static getYear(filename) {
+		let rst = /(\d{4})/.exec(filename)
+		if (rst && rst[0] !== "1080") {
+			return rst[0]
+		}
+		return null
+	}
 
-  static getShowName(filename) {
-    if (Medoc.isEpisode(filename)) {
-      let rst = /([sS]\d{2}[eE]\d{2})/g.exec(filename);
-      let tmp = filename.substr(0, rst.index);
-      tmp = tmp.replace(/\./g, " ").trim();
+	static getShowName(filename) {
+		if (Medoc.isEpisode(filename)) {
+			let rst = /([sS]\d{2}[eE]\d{2})/g.exec(filename)
+			let tmp = filename.substr(0, rst.index)
+			tmp = tmp.replace(/\./g, " ").trim()
 
-      // Extract year from name
-      let year = Medoc.getYear(filename);
+			// Extract year from name
+			let year = Medoc.getYear(filename)
 
-      if (year) {
-        tmp = tmp.replace(year, "");
-        tmp += `(${year})`;
-      }
+			if (year) {
+				tmp = tmp.replace(year, "")
+				tmp += `(${year})`
+			}
 
-      // Extract sub team from name
-      let subname = /([[\s\S]*])[\s\S]*/g.exec(tmp);
+			// Extract sub team from name
+			let subname = /([[\s\S]*])[\s\S]*/g.exec(tmp)
 
-      if (subname && subname[0]) {
-        tmp = tmp.substr(subname[0].length, tmp.length);
-      }
+			if (subname && subname[0]) {
+				tmp = tmp.substr(subname[0].length, tmp.length)
+			}
 
-      subname = /www *[\w]+ *[org|com]+[\s-]*/g.exec(tmp);
+			subname = /www *[\w]+ *[org|com]+[\s-]*/g.exec(tmp)
 
-      if (subname && subname[0]) {
-        tmp = tmp.substr(subname[0].length, tmp.length);
-      }
+			if (subname && subname[0]) {
+				tmp = tmp.substr(subname[0].length, tmp.length)
+			}
 
-      tmp = tmp.trim();
+			tmp = tmp.trim()
 
-      return tmp;
-    }
+			return tmp
+		}
 
-    return null;
-  }
+		return null
+	}
 
-  static getEpisodeSeason(filename) {
-    let rst = /[sS](\d{2})[eE]\d{2}/g.exec(filename);
+	static getEpisodeSeason(filename) {
+		let rst = /[sS](\d{2})[eE]\d{2}/g.exec(filename)
 
-    if (rst) {
-      return rst[1];
-    }
-  }
+		if (rst) {
+			return rst[1]
+		}
+	}
 
-  static getEpisodeNumber(filename) {
-    let rst = /[sS]\d{2}[eE](\d{2})/g.exec(filename);
+	static getEpisodeNumber(filename) {
+		let rst = /[sS]\d{2}[eE](\d{2})/g.exec(filename)
 
-    if (rst) {
-      return rst[1];
-    }
-  }
+		if (rst) {
+			return rst[1]
+		}
+	}
 
-  static getFile(url) {
-    if (fs.lstatSync(url).isDirectory()) {
-      let files = fs.readdirSync(url);
+	static getFile(url) {
+		if (fs.lstatSync(url).isDirectory()) {
+			let files = fs.readdirSync(url)
 
-      let filteredFiles = files.filter((file) => {
-        if (VIDEO_FORMATS.includes(path.extname(file))) {
-          return true;
-        }
-      });
+			let filteredFiles = files.filter((file) => {
+				if (VIDEO_FORMATS.includes(path.extname(file))) {
+					return true
+				}
+			})
 
-      return filteredFiles[0];
-    } else {
-      return url;
-    }
-  }
+			return filteredFiles[0]
+		} else {
+			return url
+		}
+	}
 
-  static getOriginDirectory(episode) {
-    return `${episode.origin.root}\\${episode.origin.directory}`;
-  }
+	static getOriginDirectory(episode) {
+		return `${episode.origin.root}\\${episode.origin.directory}`
+	}
 
-  static getOriginPath(episode) {
-    return `${episode.origin.root}\\${episode.origin.directory}\\${episode.origin.file}`;
-  }
+	static getOriginPath(episode) {
+		return `${episode.origin.root}\\${episode.origin.directory}\\${episode.origin.file}`
+	}
 
-  static getDestinationDirectory(episode) {
-    return `${episode.destination.root}\\${episode.destination.directory}`;
-  }
+	static getDestinationDirectory(episode) {
+		return `${episode.destination.root}\\${episode.destination.directory}`
+	}
 
-  static search(from, to) {
-    let list = [];
+	static search(from, to) {
+		let list = []
 
-    try {
-      const files = fs.readdirSync(from);
+		try {
+			const files = fs.readdirSync(from)
 
-      if (files && files.length > 0) {
-        files.map((filename) => {
-          if (Medoc.isEpisode(filename)) {
-            let filePath = `${from}\\${filename}`;
-            let isDirectory = fs.lstatSync(filePath).isDirectory();
-            let isFile = fs.lstatSync(filePath).isFile();
-            let showName = Medoc.getShowName(filename);
-            let season = Number(Medoc.getEpisodeSeason(filename));
-            let number = Number(Medoc.getEpisodeNumber(filename));
-            let format = isDirectory
-              ? path.extname(Medoc.getFile(filePath))
-              : path.extname(filename);
+			if (files && files.length > 0) {
+				files.map((filename) => {
+					if (Medoc.isEpisode(filename)) {
+						let filePath = `${from}\\${filename}`
+						let isDirectory = fs.lstatSync(filePath).isDirectory()
+						let isFile = fs.lstatSync(filePath).isFile()
+						let showName = Medoc.getShowName(filename)
+						let season = Number(Medoc.getEpisodeSeason(filename))
+						let number = Number(Medoc.getEpisodeNumber(filename))
+						let format = isDirectory
+							? path.extname(Medoc.getFile(filePath))
+							: path.extname(filename)
 
-            list.push({
-              origin: {
-                directory: fs.lstatSync(filePath).isDirectory()
-                  ? filename
-                  : null,
-                file: Medoc.getFile(filePath),
-                format: format,
-                isDirectory: isDirectory,
-                isFile: isFile,
-                path: fs.lstatSync(filePath).isDirectory()
-                  ? path.normalize(`${filePath}\\${Medoc.getFile(filePath)}`)
-                  : filePath,
-                root: from,
-              },
-              episode: {
-                show: showName,
-                season: season,
-                number: number,
-                fullname: `${showName} - ${season}x${
-                  number < 10 ? "0" + number : number
-                }`,
-              },
-              destination: {
-                directory: path.normalize(`${showName}\\Season ${season}`),
-                filename: `${showName} - ${season}x${
-                  number < 10 ? "0" + number : number
-                }${format}`,
-                path: path.normalize(
-                  `${to}\\${showName}\\Season ${season}\\${showName} - ${season}x${
-                    number < 10 ? "0" + number : number
-                  }${format}`
-                ),
-                root: to,
-              },
-            });
-          }
-        });
-      }
+						list.push({
+							origin: {
+								directory: fs.lstatSync(filePath).isDirectory()
+									? filename
+									: null,
+								file: Medoc.getFile(filePath),
+								format: format,
+								isDirectory: isDirectory,
+								isFile: isFile,
+								path: fs.lstatSync(filePath).isDirectory()
+									? path.normalize(`${filePath}\\${Medoc.getFile(filePath)}`)
+									: filePath,
+								root: from,
+							},
+							episode: {
+								show: showName,
+								season: season,
+								number: number,
+								fullname: `${showName} - ${season}x${
+									number < 10 ? "0" + number : number
+								}`,
+							},
+							destination: {
+								directory: path.normalize(`${showName}\\Season ${season}`),
+								filename: `${showName} - ${season}x${
+									number < 10 ? "0" + number : number
+								}${format}`,
+								path: path.normalize(
+									`${to}\\${showName}\\Season ${season}\\${showName} - ${season}x${
+										number < 10 ? "0" + number : number
+									}${format}`
+								),
+								root: to,
+							},
+						})
+					}
+				})
+			}
 
-      return list;
-    } catch (e) {
-      console.error(e);
-      return new Error(
-        `{ "message": "Directory ${from} is not accessible !", "stack" : "${e}"`
-      );
-    }
-  }
-};
+			return list
+		} catch (e) {
+			console.error(e)
+			return new Error(
+				`{ "message": "Directory ${from} is not accessible !", "stack" : "${e}"`
+			)
+		}
+	}
+}
--- test/server.js
+++ test/server.js
@@ -1,18 +1,14 @@
-const Medoc = require("../medoc");
+const Medoc = require("../medoc")
 
-// const PATH_TO_SCAN = `w:`
-// const PATH_TO_PUSH = `z:`
-const PATH_TO_SCAN = `u:`;
-const PATH_TO_PUSH = `q:`;
+const PATH_TO_SCAN = "u:"
+const PATH_TO_PUSH = "q:"
 
-// const episodes = Medoc.search(PATH_TO_SCAN, PATH_TO_PUSH)
-
-const medoc = new Medoc(PATH_TO_SCAN, PATH_TO_PUSH);
+const medoc = new Medoc(PATH_TO_SCAN, PATH_TO_PUSH)
 medoc
-  .runSync()
-  .then((results) => {
-    console.log("Terminé :", results);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+	.runSync()
+	.then((results) => {
+		console.log("Terminé :", results)
+	})
+	.catch((err) => {
+		console.error(err)
+	})
